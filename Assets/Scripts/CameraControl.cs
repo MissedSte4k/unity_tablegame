39e7d7f4ea4211f5300c914161c26abcc2622f6e
MissedSte4k <36638029+MissedSte4k@users.noreply.github.com>
2018-02-21 21:01:02 +0200

    Pirmas įkėlimas
    Padarytas vaikščiojimas su primityviais blokais

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	private float mouseH = 0.0f;
+	private float mouseV = 0.0f;
+	public float mouseSensitivity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouseH += Input.GetAxis ("Mouse X") * mouseSensitivity;
+		mouseV -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+
+		transform.rotation = Quaternion.Euler (Mathf.Clamp(mouseV, -90, 90), mouseH, 0);
+	}
+}
---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+	
+	public float moveSpeed;
+	public float crouchSpeedReduction;
+	public float mouseSpeed;
+	public float jumpHeight;
+	public GameObject playerCamera;
+	private Rigidbody rb;
+	private bool isCrouched = false;
+	private bool onGround = true;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update() {
+		
+		transform.rotation = Quaternion.Euler (0, playerCamera.transform.rotation.eulerAngles.y, 0);
+
+		if (Input.GetButtonDown ("Crouch")) {
+			if (isCrouched == false) {
+				isCrouched = true;
+				transform.localScale = new Vector3 (transform.localScale.x, transform.localScale.y * 0.5f, transform.localScale.z);
+				transform.position = new Vector3 (transform.position.x, transform.localScale.y * 0.5f, transform.position.z);
+				moveSpeed -= crouchSpeedReduction;
+			} else {
+				isCrouched = false;
+				transform.localScale = new Vector3 (transform.localScale.x, transform.localScale.y * 2, transform.localScale.z);
+				transform.position = new Vector3 (transform.position.x, transform.localScale.y * 0.5f, transform.position.z);
+				moveSpeed += crouchSpeedReduction;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 forward = transform.forward * moveSpeed * moveVertical;
+		Vector3 horizontal = transform.right * moveSpeed * moveHorizontal;
+
+		rb.velocity = forward + horizontal + new Vector3 (0, rb.velocity.y, 0);
+
+		if (Input.GetButtonDown ("Jump") && onGround) {
+			rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+		}
+			
+	}
+
+	private void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.CompareTag ("Ground")) {
+			onGround = true;
+		}
+	}
+
+	private void OnCollisionExit (Collision collision) {
+		if (collision.gameObject.CompareTag ("Ground")) {
+			onGround = false;
+		}
+	}
+
+	private void OnCollisionStay (Collision collision) {
+		if (collision.gameObject.CompareTag ("Ground")) {
+			onGround = true;
+		}
+	}
+}
