f467730a607c8cdbd1614abe864ab54784f6a0a9
Ignas236 <lunys.ignas@gmail.com>
2018-03-20 15:41:05 +0200

    Pilnai valdomas Player su visom animacijom

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -10,62 +10,118 @@ public class CharacterControl : NetworkBehaviour {
 	public float crouchSpeedReduction;
 	public float jumpHeight;
 	private Rigidbody rb;
+	private CapsuleCollider hitBox;
 	private bool isCrouched = false;
-    private bool isJumped = false;
 	private bool onGround = true;
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public int bulletSpeed;
-    public Text healthText;
     private float mouseH = 0.0f;
     private float mouseV = 0.0f;
     public float mouseSensitivity;
     public Camera playerCamera;
+	public float smoothing = 5f;
+	NetworkAnimator anim;
+	// dvi apatinės skeleto stuburo dalys, naudojamos žiūrėt aukštyn/žemyn
+	public Transform spine;
+	public Transform spine1;
 
 
     // Use this for initialization
     void Start () {
 		rb = GetComponent<Rigidbody> ();
+		anim = GetComponent<NetworkAnimator> ();
+		hitBox = GetComponent<CapsuleCollider> ();
 	}
 
 	void Update() {
         if (isLocalPlayer)
         {
-            //Health health = GetComponent<Health>();
-            //healthText.text = "Health: " + health.CurrentHealth();
-            transform.rotation = Quaternion.Euler(playerCamera.transform.rotation.eulerAngles.x, playerCamera.transform.rotation.eulerAngles.y, playerCamera.transform.rotation.eulerAngles.z);
+			anim.animator.ResetTrigger ("Attack2");
+			anim.animator.ResetTrigger ("Attack3");
+
+			anim.animator.SetFloat ("Speed", Input.GetAxis ("Vertical"));
+			anim.animator.SetFloat ("Strafe", Input.GetAxis ("Horizontal"));
+
+            transform.rotation = Quaternion.Euler(0, playerCamera.transform.rotation.eulerAngles.y, 0);
 
             mouseH += Input.GetAxis("Mouse X") * mouseSensitivity;
             mouseV -= Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-            playerCamera.transform.rotation = Quaternion.Euler(Mathf.Clamp(mouseV, -90, 90), mouseH, 0);
+            playerCamera.transform.rotation = Quaternion.Euler(Mathf.Clamp(mouseV, -60, 60), mouseH, 0);
+
+
+			if (isCrouched == false) {
+				hitBox.height = Mathf.Lerp (hitBox.height, 2.2f, Time.deltaTime * 5);
+				hitBox.center = new Vector3 (0, Mathf.Lerp(hitBox.center.y, 0.1f, Time.deltaTime * 5), 0);
+
+			} else {
+				hitBox.height = Mathf.Lerp (hitBox.height, 1.5f, Time.deltaTime * 5);
+				hitBox.center = new Vector3 (0, Mathf.Lerp(hitBox.center.y, 0.25f, Time.deltaTime * 5), 0);
+			}
 
             if (Input.GetButtonDown("Crouch"))
             {
                 if (isCrouched == false)
                 {
                     isCrouched = true;
-                    transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 0.5f, transform.localScale.z);
-                    transform.position = new Vector3(transform.position.x, transform.localScale.y * 0.5f, transform.position.z);
                     moveSpeed -= crouchSpeedReduction;
+					anim.animator.SetBool ("Crouched", true);
                 }
                 else
                 {
                     isCrouched = false;
-                    transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 2, transform.localScale.z);
-                    transform.position = new Vector3(transform.position.x, transform.localScale.y * 0.5f, transform.position.z);
                     moveSpeed += crouchSpeedReduction;
+					anim.animator.SetBool ("Crouched", false);
                 }
             }
 
-            if(Input.GetButtonDown("Fire1"))
-            {
-                CmdFire();
-            }
+			// ataka vyksta kol laikomas nuspaustas mygtukas
+			if (Input.GetButton ("Fire1")) {
+				anim.animator.SetBool ("Attack1", true);
+			} else {
+				anim.animator.SetBool ("Attack1", false);
+			}
+
+			// ataka vyksta 1 kartą nuspaudus mygtuką
+			if(Input.GetButtonDown("Fire2"))
+			{
+				anim.SetTrigger ("Attack2");
+				CmdFire();
+			}
+
+			// ataka vyksta 1 kartą nuspaudus mygtuką
+			if(Input.GetButtonDown("Fire3"))
+			{
+				anim.SetTrigger ("Attack3");
+				CmdFire();
+			}
+
+			// ataka vyksta 1 kartą nuspaudus mygtuką
+			if(Input.GetButtonDown("Fire4"))
+			{
+				anim.SetTrigger ("Slam");
+				CmdFire();
+			}
+
+			// blokas vykdomas tol, kol laikomas nuspaustas mygtukas
+			if (Input.GetButton ("Block")) {
+				anim.animator.SetBool ("Block", true);
+			} else {
+				anim.animator.SetBool ("Block", false);
+			}
 
         }
         else playerCamera.enabled = false;
     }
+
+	void LateUpdate(){
+		if (isLocalPlayer) {
+			// vykdoma modelio stuburo rotacija aukštyn/žemyn, kad atrodytu lyg veikėjas ten žiūri
+			spine.rotation = Quaternion.Euler (spine.rotation.eulerAngles.x, spine.rotation.eulerAngles.y, spine.rotation.eulerAngles.z - Mathf.Clamp (mouseV, -60, 60) / 2); 
+			spine1.rotation = Quaternion.Euler (spine.rotation.eulerAngles.x, spine.rotation.eulerAngles.y, spine.rotation.eulerAngles.z - Mathf.Clamp (mouseV, -60, 60) / 2);
+		}
+	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -99,7 +155,9 @@ public class CharacterControl : NetworkBehaviour {
         {
             if (collision.gameObject.CompareTag("Ground"))
             {
-                    onGround = true;
+                onGround = true;
+				anim.animator.SetBool ("Jump", false);
+				anim.animator.SetBool ("Falling", false);
             }
         }
 	}
@@ -110,6 +168,10 @@ public class CharacterControl : NetworkBehaviour {
             if (collision.gameObject.CompareTag("Ground"))
             {
                 onGround = false;
+				if (Input.GetButtonDown ("Jump")) {
+					anim.animator.SetBool ("Jump", true);
+				}
+				anim.animator.SetBool ("Falling", true);
             }
         }
 	}
@@ -120,6 +182,8 @@ public class CharacterControl : NetworkBehaviour {
             if (collision.gameObject.CompareTag("Ground"))
             {
                 onGround = true;
+				anim.animator.SetBool ("Jump", false);
+				anim.animator.SetBool ("Falling", false);
             }
         }
 	}
