cdd34a16efc4042c27becec71acbf2c6d3bae910
Ignas236 <zappah5@gmail.com>
2018-04-28 03:27:24 +0300

    WIP weapon attacks

---
+++ Assets/Scripts/ImmobilisingTrap.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmobilisingTrap : MonoBehaviour {
+
+    public int stopTime;
+    public int lifetime;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+
+        if (hit.CompareTag("Ground"))
+        {
+            rb.isKinematic = true;
+            rb.velocity = Vector3.zero;
+        }
+
+        if (hit.CompareTag("Player"))
+        {
+            hit.GetComponent<Rigidbody>().isKinematic = true;
+            hit.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            StartCoroutine(Stop(stopTime));
+            Destroy(gameObject);
+        }
+
+        StartCoroutine(Stop(lifetime));
+    }
+
+    IEnumerator Stop(int time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Projectile : NetworkBehaviour {
+
+    public int damage;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Quaternion rotation = transform.rotation;
+        rotation.SetLookRotation(rb.velocity);
+        transform.rotation = rotation;
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        
+        var health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.CmdTakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
