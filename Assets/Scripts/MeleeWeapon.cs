0275417ab813f27353ca346a26a082fd568d76e9
Ignas236 <zappah5@gmail.com>
2018-04-28 21:54:37 +0300

    beep beep

---
+++ Assets/Scripts/MeleeWeapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MeleeWeapon : NetworkBehaviour {
+
+    public int damage;
+    public bool isSlash; //marks whether the weapon can damage more than 1 character.
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var health = other.gameObject.GetComponent<Health>();
+            if (health != null)
+            {
+                health.CmdTakeDamage(damage);
+            }
+            if (!isSlash)
+                GetComponent<Collider>().enabled = false;
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,25 +6,18 @@ using UnityEngine.Networking;
 public class Projectile : NetworkBehaviour {
 
     public int damage;
-    Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Quaternion rotation = transform.rotation;
-        rotation.SetLookRotation(rb.velocity);
-        transform.rotation = rotation;
 	}
 
     void OnCollisionEnter(Collision collision)
     {
-        var hit = collision.gameObject;
-        
-        var health = hit.GetComponent<Health>();
+        var health = collision.gameObject.GetComponent<Health>();
         if (health != null)
         {
             health.CmdTakeDamage(damage);
