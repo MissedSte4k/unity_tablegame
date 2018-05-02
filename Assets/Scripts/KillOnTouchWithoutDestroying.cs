5ed538cb6a7630cb7426f7a9501fb3ed3332d294
Ignas236 <zappah5@gmail.com>
2018-05-03 00:13:32 +0300

    Minor fixes

--- Assets/Scripts/KillOnTouchWithoutDestroying.cs
+++ Assets/Scripts/KillOnTouchWithoutDestroying.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class KillOnTouch : NetworkBehaviour {
+public class KillOnTouchWithoutDestroying : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
