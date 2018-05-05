2f7058cdfd936ee69af3fa94fea8b27e946061bc
Ignas236 <zappah5@gmail.com>
2018-05-05 03:50:54 +0300

    Ever get that feeling of Deja Vu?
    Also WIP Scout

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Bomb : NetworkBehaviour {
+
+    public float explosionRadius;
+    public GameObject[] models;
+    [SyncVar]
+    public NetworkInstanceId spawnedBy;
+    private Rigidbody hitrb;
+    public Vector3 spin;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject obj = ClientScene.FindLocalObject(spawnedBy);
+        Physics.IgnoreCollision(GetComponent<Collider>(), obj.GetComponent<Collider>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Quaternion angleRotation = Quaternion.Euler(spin * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * angleRotation);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       
+    }
+
+
+}
